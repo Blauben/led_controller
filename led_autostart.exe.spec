223673ff61fec1129f5efc7b712b211e8dc105f8
unknown <ben@amatala.de>
2025-07-31 04:39:21 +0200

    Progress

---
+++ led_autostart.exe.spec
@@ -0,0 +1,44 @@
+# -*- mode: python ; coding: utf-8 -*-
+from setuptools_scm import get_version
+
+version = "0.0.0"
+try:
+    version = get_version()
+except Exception as e:
+    print(e)
+
+a = Analysis(
+    ['main.py'],
+    pathex=[],
+    binaries=[],
+    datas=[],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+    optimize=0,
+)
+pyz = PYZ(a.pure)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.datas,
+    [],
+    name=f'led_autostart-{version}.exe',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=True,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+)
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import datetime
 import random
+import signal
 import sys
+import traceback
+from asyncio import CancelledError
 
 from bleak import BleakClient, BleakError, BleakScanner
 import yaml
@@ -7,11 +11,20 @@ import asyncio
 
 # Load config from the yaml file
 config = yaml.safe_load(open("config.yml", "r").read())
+signal_received = asyncio.Event()
+
+client: BleakClient | None = None
 
 
-async def send_command(client: BleakClient, command: bytes) -> None:
-    print(f"Sending command: {command}")
-    await client.write_gatt_char(config["gatt_char_uuid"], command, response=False)
+async def send_command(command: bytes) -> None:
+    global client
+    try:
+        print(f"{timestamp()} Sending command: {command}")
+        await client.write_gatt_char(config["gatt_char_uuid"], command, response=False)
+    except BleakError as e:
+        print(f"{timestamp()} BleakError: {e}, Command failed. Reestablishing connection.")
+        await led_connect_loop()
+        await send_command(command)
 
 
 def color_command(rgb_hex: str) -> bytes:
@@ -22,37 +35,76 @@ def power_command(turnOn: bool) -> bytes:
     return bytes.fromhex("7e0404f00001ff00ef" if turnOn else "7e0404000000ff00ef")
 
 
-async def led_connect() -> BleakClient:
-    print(f"Connecting to BLE LED strip.")
-    device = await BleakScanner.find_device_by_address(config["led_mac"], timeout=config["connection_timeout_sec"])
-    print("Device discovered.")
-    client = BleakClient(device)
-    while True:
+async def discover_device():
+    return await BleakScanner.find_device_by_address(config["led_mac"], timeout=config["connection_timeout_sec"])
+
+
+async def led_connect_loop():
+    for _ in range(int(config["connection_retries"])):
+        try:
+            await led_connect()
+            break
+        except Exception as e:
+            print(
+                f"{timestamp()} Could not connect to BLE LED. Is there an existing or old connection? Try restarting bluetooth. Error: {e}",
+                file=sys.stderr)
+
+
+async def await_connection():
+    for _ in range(3):
         try:
             await client.connect(timeout=config["connection_timeout_sec"])
             break
-        except BleakError:
-            print("Could not connect to BLE LED. Is there an existing or old connection? Try restarting bluetooth.", file=sys.stderr)
+        except Exception as e:
+            print(f"{timestamp()} BLE Device Service unavailable ({e}). Trying again.", file=sys.stderr)
+
+
+async def led_connect() -> None:
+    print(f"{timestamp()} Connecting to BLE LED strip.")
+    device = await discover_device()
+    print(f"{timestamp()} Device discovered.")
+    global client
+    client = BleakClient(device)
+    await await_connection()
 
     if client.is_connected:
-        print("Connected!")
-        return client
+        print(f"{timestamp()} Connected!")
     else:
         raise ConnectionError(
-            f"BLE Connection to {config["led_mac"]} failed after {config["connection_timeout_sec"]} seconds.")
+            f"{timestamp()} BLE Connection to {config["led_mac"]} failed after several retries.")
+
+
+def timestamp():
+    return datetime.datetime.now().strftime("%H:%M:%S")
+
+
+async def schedule_cleanup() -> None:
+    await signal_received.wait()
+    print(f"{timestamp()} Cleaning up.")
+    global client
+    await send_command(power_command(turnOn=False))
+    await client.disconnect()
+
+
+def sighandler(signum, frame) -> None:
+    print(f"{timestamp()} Exiting.")
+    signal_received.set()
 
 
 async def main():
-    print(f"Using config param {config}")
-    connection = await led_connect()
+    print(f"{timestamp()} Using config parameters: {config}")
+    await led_connect_loop()
+    signal.signal(signal.SIGINT, sighandler)  # Ctrl+C
+    signal.signal(signal.SIGTERM, sighandler)  # Termination signal
+    signal.signal(signal.SIGBREAK, sighandler)  # Ctrl+Break
     try:
-        await send_command(connection, power_command(turnOn=True))
+        asyncio.create_task(schedule_cleanup())
+        await send_command(power_command(turnOn=True))
         while True:
-            await send_command(connection, color_command(rgb_hex=random.randbytes(3).hex()))
+            await send_command(color_command(rgb_hex=random.randbytes(3).hex()))
             input("Press Enter to change color...")
     finally:
-        await send_command(connection, power_command(turnOn=False))
-        await connection.disconnect()
+        signal_received.set()
 
 
 if __name__ == "__main__":

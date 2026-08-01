17db5ba734cf2ee791c9a1185e8f21d3d81b4531
Blauben <72825696+Blauben@users.noreply.github.com>
2026-08-01 20:54:49 +0200

    Improved argsparsing; allow piping multiple instructions and enable randomzing on autostart

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import argparse
 import os.path
 import random
 from datetime import date, datetime as dt
 
 import logging.handlers
+import re
 import signal
 from functools import partial
 from collections.abc import Callable, Awaitable
@@ -56,6 +58,10 @@ def build_help_page(command_map: dict[str, CommandEntry]) -> str:
 
 
 def async_command_map(driver: LEDDriver, *args: Any) -> dict[str, CommandEntry]:
+    random_color = CommandEntry(
+        "Send a random color",
+        partial(driver.send_command, color_command(rgb_hex=random.randbytes(3).hex())),
+    )
     command_map: dict[str, CommandEntry] = {
         "off": CommandEntry(
             "Turn the LED off",
@@ -73,12 +79,8 @@ def async_command_map(driver: LEDDriver, *args: Any) -> dict[str, CommandEntry]:
             partial(driver.send_command, brightness_command(*args)),
         ),
         "q": CommandEntry("Quit the controller", quit_main_loop),
-        "": CommandEntry(
-            "Send a random color",
-            partial(
-                driver.send_command, color_command(rgb_hex=random.randbytes(3).hex())
-            ),
-        ),
+        "": random_color,
+        "r": random_color,
     }
     command_map["help"] = CommandEntry(
         "Show this help page", partial(print_message, build_help_page(command_map))
@@ -119,15 +121,17 @@ def register_sighandlers(driver: LEDDriver):
     signal.signal(signal.SIGTERM, partial(sighandler, driver))
 
 
-async def autostart_at_sunset_job(driver: LEDDriver):
+async def autostart_at_sunset_job(driver: LEDDriver, args: argparse.Namespace):
     await async_command_map(driver)["on"].command()
+    if args.randomize:
+        await async_command_map(driver)["r"].command()
     now = dt.now()
     poweroff_minutes = max(30, 24 * 60 - (now.hour * 60 + now.minute))
     logger.info("Powering off in %d minutes", poweroff_minutes)
     await async_command_map(driver, poweroff_minutes)["s"].command()
 
 
-async def run_autostart_at_sunset(driver: LEDDriver):
+async def run_autostart_at_sunset(driver: LEDDriver, args: argparse.Namespace):
     sunset_time = dt.strptime(get_sunset(), "%I:%M:%S %p").time()
     sunset = dt.combine(date.today(), sunset_time)
     logger.debug("Sunset time: %s", sunset)
@@ -141,7 +145,7 @@ async def run_autostart_at_sunset(driver: LEDDriver):
         )
         await asyncio.sleep(wait_seconds)
 
-    await autostart_at_sunset_job(driver)
+    await autostart_at_sunset_job(driver, args)
 
 
 async def execute_instr(driver: LEDDriver, instr: str):
@@ -156,9 +160,9 @@ async def execute_instr(driver: LEDDriver, instr: str):
         pass
 
 
-async def handle_cli_args(driver: LEDDriver):
-    if "--autostart" in sys.argv:
-        await run_autostart_at_sunset(driver)
+async def handle_cli_args(driver: LEDDriver, args: argparse.Namespace):
+    if args.autostart:
+        await run_autostart_at_sunset(driver, args)
 
 
 async def cli_loop(driver: LEDDriver):
@@ -168,22 +172,68 @@ async def cli_loop(driver: LEDDriver):
         await execute_instr(driver, instr)
 
 
+async def parse_stdin_stream(driver: LEDDriver, instructions: str = ""):
+    pruned_instructions = (
+        re.sub(r"\s+", " ", instructions)
+        .replace("\n", "")
+        .replace("\r", "")
+        .replace('"', "")
+        .replace("'", "")
+    )
+    for instr in pruned_instructions.split(";"):
+        instr = instr.strip()
+        if instr == "":
+            continue
+        await execute_instr(driver, instr)
+
+
+async def argparser(driver: LEDDriver):
+    parser = argparse.ArgumentParser(
+        prog="LED_Controller",
+        description="This program is a BLE LED Controller for ELKBLE-DOM LED devices. This program allows you to control the LED device via command-line interface (CLI) or by piping commands directly into the program as well as functioning as an autostart daemon to start the LED at sunset.",
+        epilog=f"""\
+    If no arguments are provided, the program will enter an interactive command-line interface (CLI) mode.
+    In this mode, you can enter commands to control the LED device.
+    Commands can be entered one at a time, or multiple commands can be separated by semicolons (;).
+
+    Alternatively, you can pipe commands seperated by semicolons (;) directly into the program. For example:
+    echo "on; s 60; c" | python main.py
+
+    {build_help_page(async_command_map(driver))}
+
+    Authored by: Ben Frauenknecht""",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "--autostart",
+        action="store_true",
+        help="Automatically start the LED at sunset and schedule power-off at midnight.",
+    )
+    parser.add_argument(
+        "--randomize",
+        action="store_true",
+        help="Randomize the LED color on startup. This option is only effective when used with --autostart.",
+    )
+    return parser.parse_args()
+
+
 async def main():
+    driver = LEDDriver()
+    args = await argparser(driver)
     logger.info("Starting LED Controller")
     generate_config()
-    driver = LEDDriver()
     logger.info("Using config parameters: %s", driver.config)
     register_sighandlers(driver)
     await driver.led_connect_loop()
     if not sys.stdin.isatty():
         logger.debug(f"Reading from stdin...")
-        instr = sys.stdin.read().replace("\n", "").replace("\r", "").strip()
+        instr = sys.stdin.read()
         logger.debug(
             f"Received piped data: %s. Treating as instruction and exiting...", instr
         )
-        await execute_instr(driver, instr)
+        await parse_stdin_stream(driver, instr)
     elif len(sys.argv) > 1:
-        await handle_cli_args(driver)
+        await handle_cli_args(driver, args)
     else:
         await cli_loop(driver)
     logger.info("Exiting...")
